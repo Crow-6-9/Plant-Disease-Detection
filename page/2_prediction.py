84628d0d9a739dcf0c0c087225436046300ded19
Stanley Correia <stanley.correia@indigochart.com>
2025-02-03 01:39:47 +0530

    Game 2 Game

--- page/2_prediction.py
+++ page/2_prediction.py
@@ -67,6 +67,19 @@ def load_model():
     
     return model
 
+@lru_cache()
+def model_prediction(test_image_path):
+    global model_path
+    model = load_model()
+    
+    image = tf.keras.preprocessing.image.load_img(test_image_path, target_size=(128, 128))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.expand_dims(input_arr, axis=0)
+    prediction = model.predict(input_arr)
+    result_index = np.argmax(prediction)
+
+    return result_index
+
 test_image = st.file_uploader("Choose an image:", type=["jpg", "jpeg", "png"])
 
 if test_image:
